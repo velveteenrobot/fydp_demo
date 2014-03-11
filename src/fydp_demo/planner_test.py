c3693087e0995e7b70783bda0b33e166f9916984
Sarah Elliott <sksellio@gmail.com>
2014-03-11 01:52:21 -0400

    Updating planner_test

--- src/fydp_demo/planner_test.py
+++ src/fydp_demo/planner_test.py
@@ -3,6 +3,8 @@ import rospy
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseArray
 from std_msgs.msg import Bool
+from nav_msgs.msg import OccupancyGrid
+from fydp_demo.msg import ips_msg
 
 import numpy as np
 import scipy as sp
@@ -12,81 +14,171 @@ import matplotlib
 from matplotlib import animation
 import matplotlib.pyplot as plt
 
-import vision_test
-import utils
-import Robot
+import mtRobot as mtRobot
 
 from numpy import int64
 from numpy import int16
 
-m = vision_test.Maps(dsample_scale=1.00)
-m.load_map('IGVCmap3.tif')
+global map_ready
+map_ready = False
+global map_size
+global robot
+global map_res
 
-
-robot = Robot.Robot(m)
-robot.test_load()
-robot.load_planner()
-
-#robot.waypoints.append((100,300))
-
-#robot.current_vision = vision_test.get_robot_vision(m, robot.loc[0], robot.loc[1], robot.srad)
-#robot.all_vision = robot.current_vision
-
-cnt = 0
-done = False
-
-record = []
-record2 = []
-record3 = []
-record4 = []
-
-pub = rospy.Publisher('waypoints', PoseArray)
-pub2 = rospy.Publisher('waypoints_done', Bool)
-rospy.init_node('global_planner')
-
-while not done:
-    #raw_input('CLICK TO GLOBAL PLAN!')
-    robot.test_step()
-    
-    if robot.path.shape[0] < 11:
-        robot.loc = robot.path[-1,:]
+def map_callback(data):
+    global map_ready
+    global robot
+    global map_size
+    global map_res
+    if map_ready:
+        grid = np.zeros((data.info.height, data.info.width))
+        for i in range(data.info.width):
+            for j in range(data.info.height):
+                if data.data[data.info.width*j + i] < 0.5:
+                    grid[i,j] = 0
+                elif data.data[data.info.width*j + i] == -1:
+                    grid[i,j] = 0
+                else:
+                    grid[i,j] = 1
+        if robot.prev_map is not None:
+            delta = np.logical_xor(np.asarray(robot.prev_map), grid)
+            robot.update_vision_gazebo(delta)
+            robot.temp_stupid = grid
+        robot.add_prev_map(grid)
     else:
-        robot.loc = robot.path[10,:]
+        map_size = (data.info.height, data.info.width)
+        map_ready = True
+        map_res = float(data.info.resolution)
+
+
+def position_callback(data):
+    global map_ready
+    global robot
+    global map_res
+    if map_ready:
+        location = []
+        # coordinates in the gazebo frame of reference
+        gazebo_y = data.Y;  gazebo_x = data.X
+        # refer to turtlebot_example where we get shifted_point for the inverse math
+        # now convert to planner frame of reference
+        planner_x = int(np.round(-gazebo_x / map_res + float(map_size[1]) / 2) )
+        planner_y = int(np.round(float(map_size[0]) / 2 + gazebo_y / map_res) )
+
+        print gazebo_x, gazebo_y, planner_x, planner_y
+
+        location.append(planner_y); location.append(planner_x)
+        robot.update_position(location)
+    else:
+        pass
+
+if __name__ == '__main__':
+
+    global robot
+    robot = mtRobot.mtRobot(simulation=False)
+
+    MAP_TOPIC = '/map'
+
+    pub = rospy.Publisher('waypoints', PoseArray)
+    pub2 = rospy.Publisher('waypoints_done', Bool)
+
+    map_sub = rospy.Subscriber(MAP_TOPIC, OccupancyGrid, map_callback)
+    position_sub = rospy.Subscriber('/indoor_pos', ips_msg, position_callback)
+    rospy.init_node('global_planner')
+    while map_ready is False:
+        rospy.sleep(1)
+    
+    robot.test_load()
+    robot.add_simulated_sensor('IGVC-lane.tif', 25, 0, 'lanes', occluded=False)
+    #robot.add_simulated_sensor('IGVC-obs.tif', 25, 0, 'obstacles', occluded=True)
+    robot.sensors[0]._map = np.zeros(map_size)
+    robot.combined_delta_bound_edges = np.zeros(map_size)
+    robot.load_planner()
+
+
+    VID_OUT = True
+
+    #robot.waypoints.append((100,300))path
+
+    #robot.current_vision = vision_test.get_robot_vision(m, robot.loc[0], robot.loc[1], robot.srad)
+    #robot.all_vision = robot.current_vision
+
+    cnt = 0
+    done = False
+    cnt = 0
+
+
+
+
+
+    while not done:
+        print "IN MAIN LOOP ", cnt
+        robot.test_step()
+        if robot.temp_stupid is not None:
+            plt.figure()
+            name = "vidout-%03d.png" %cnt
+            plt.imshow(robot.temp_stupid)
+            plt.savefig(name)
+            plt.close()
+
+        """
+        plt.figure()
+        name = "vidout-%03d.png" %cnt
         
-    """record.append(robot.planner.key_record)
-    record2.append(robot.planner.g_score)
-    record3.append(robot.loc)
-    record4.append(robot.planner.rhs)
-    """
+        debug_map = np.logical_and(robot.sensors[0].memory_obstacle_map, robot.sensors[1].memory_obstacle_map)
+        plt.imshow(debug_map, cmap='gray')
+        plt.scatter(robot.loc[1], robot.loc[0], c='b')
+        plt.scatter(robot.path[:,1], robot.path[:,0], c='r')
+        path
+        plt.savefig(name)
+        plt.close()
+        """
+        
 
-    waypoint_array = PoseArray();
-
-    for i in range(robot.path.shape[0]):
-        waypoint = Pose()
-        waypoint.position.x = robot.path[i,1]
-        print "x: " + str(robot.path[i,1])
-        waypoint.position.y = robot.path[i,0]
-        print "y: " + str(robot.path[i,0])
-        waypoint_array.poses.append(waypoint)
-
-    pub.publish(waypoint_array)
-    rospy.sleep(3)
-
-    for i in waypoint_array.poses:
-        print i.position.x, i.position.y
-
-    #
-    if robot.is_at_current_goal():
-        if robot.progress + 1 == len(robot.waypoints):
-            waypoints_done = Bool()
-            waypoints_done.data = True
-            pub2.publish(waypoints_done)
-            print "Done"
-            done = True
+        #for i in range(robot.path.shape[0]):
+        """
+        for i in range(len(robot.path)):
+            waypoint = Pose()
+            waypoint.position.x = robot.path[i,1]
+            print "x: " + str(robot.path[i,1])
+            waypoint.position.y = robot.path[i,0]
+            print "y: " + str(robot.path[i,0])
+            waypoint_array.poses.append(waypoint)
+        """
+        """
+        if robot.path.shape[0] < 11:
+            robot.loc = robot.path[-1,:]
         else:
-            robot.progress = robot.progress + 1
-            robot.move_to_next_waypoint()
+            robot.loc = robot.path[10,:]
+        """
+        waypoint_array = PoseArray();
+        print robot.path
+
+        for i in range(robot.path.shape[0]):
+            waypoint = Pose()
+            waypoint.position.x = robot.path[i,1]
+            print "x: " + str(robot.path[i,1])
+            waypoint.position.y = robot.path[i,0]
+            print "y: " + str(robot.path[i,0])
+            waypoint_array.poses.append(waypoint)
+
+
+        for i in waypoint_array.poses:
+            print i.position.x, i.position.y
+            
+        pub.publish(waypoint_array)
+        rospy.sleep(1.5)
+        
+        if robot.is_at_current_goal():
+            if robot.progress + 1 == len(robot.waypoints):
+                waypoints_done = Bool()
+                waypoints_done.data = True
+                pub2.publish(waypoints_done)
+                done = True
+            else:
+                robot.progress = robot.progress + 1
+                robot.move_to_next_waypoint()
+        
+        cnt += 1
     
 #ani = animation.ArtistAnimation(fig, ims, interval=250, blit=True, repeat_delay=1000)
     
-        
